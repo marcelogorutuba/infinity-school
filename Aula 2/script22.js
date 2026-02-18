e9365ea0f4c851f7d48f52c637f66775df18c897
marcelogorutuba <marcelogorutuba@gmail.com>
2026-02-18 16:16:48 -0300

    Add files via upload

---
+++ Aula 2/script22.js
@@ -0,0 +1,11 @@
+let numero = Number(prompt( "Digite o numero"))
+if (numero > 0) {
+    console.log("Positivo")
+}
+else if (numero == 0){
+    console.log("Nulo")
+}
+
+else{
+    console.log("Negativo")
+}
